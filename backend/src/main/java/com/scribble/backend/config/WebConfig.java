a98e3e7491a93fecabfd4512d6e9ef4880665c53
Your Name <abhi1997shuklas@gmail.com>
2026-08-06 17:54:32 +0530

    updated cors for deployment

--- backend/src/main/java/com/scribble/backend/config/WebConfig.java
+++ backend/src/main/java/com/scribble/backend/config/WebConfig.java
@@ -10,9 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                .allowedOrigins("https://scribble-clone-bay.vercel.app", "http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
-
 }
--- backend/src/main/java/com/scribble/backend/config/WebSocketConfig.java
+++ backend/src/main/java/com/scribble/backend/config/WebSocketConfig.java
@@ -26,7 +26,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:5173");
+                .setAllowedOrigins("https://scribble-clone-bay.vercel.app", "http://localhost:5173");
     }
 
     @Override
